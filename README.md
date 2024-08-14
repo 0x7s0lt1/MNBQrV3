7ebaf3ad89c1c46697ce3ca2922227d9cf211910
0x7s0lt1 <zsoltfehervari@gmx.com>
2024-08-14 16:13:42 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# mnb-qr.js
+# MNBQrV3
+
+`MNBQrV3` is a JavaScript class designed to handle payment-related data according to the MNB QR v3 specification. It ensures that the data fields adhere to specific validation rules and formats. This class can generate JSON data for further processing.
+
+## Features
+- Field validation: All fields have associated validation rules (e.g., required fields, pattern matching).
+- JSON generation: Generates a JSON string based on the provided data.
+- Encapsulation: Internal data is stored in private fields and can only be accessed or modified through getter and setter methods.
+
+## Installation
+Include the MNBQrV3 class in your project:
+```javascript
+const MNBQrV3 = require('path/to/MNBQrV3');
+```
+
+## Usage
+### Creating an Instance
+You can initialize an instance of MNBQrV3 by passing an optional configuration object. This object should match the schema described below:
+```javascript
+const qrData = new MNBQrV3({
+    BIC: 'GIBAHUHB',
+    name: 'Example Name',
+    IBAN: 'HU12345678901234567890123456',
+    value: 1000,
+    currency: 'HUF',
+    expiry: '2024-12-31T23:59',
+    identification: 1,
+    // ... other optional fields
+});
+```
+### Methods
+- `async get(field)`: Retrieve the value of a specific field.
+- `async set(field, value)`: Set the value of a specific field after validation.
+- `async generateObject()`: Generate a JS object based on the field values.
+- `async generateJSON()`: Generate a JSON string based on the field values.
+
+#### Example
+```javascript
+const qr = new MNBQrV3();
+
+qr.setBIC("GIBAHUHB");
+qr.setIBAN("HU12345678901234567890123456");
+qr.setIdentification(MNBQrV3.HCT);
+qr.setName("John Doe");
+qr.setValue(10000);
+qr.setCurrency("HUF");
+qr.setRepeatability(MNBQrV3.REPETABLE);
+qr.setExpiry(
+    Date.parse("2024-12-31T23:59")
+);
+
+const json = qr.generateJSON();
+console.log(json);
+```
+
+### Fields
+- The MNBQrV3 class supports the following fields:
+- BIC (required): Bank Identifier Code, validated by pattern /^[a-zA-Z]{8,11}$/.
+- name (required): Name of the beneficiary, validated by pattern /^.{1,70}$/.
+- IBAN (required): International Bank Account Number, validated by pattern /^[a-zA-Z0-9]{28}$/.
+- value (required): Amount, validated as a number with a pattern /^(10{7}|[1-9]\d{0,6}|0)$/.
+- currency (required): Currency code, validated by pattern /^[a-zA-Z]{3}$/.
+- expiry (required): Expiry date in ISO 8601 format.
+- identification (required): Numeric identification.
+- text: Optional text field.
+- positionId: Optional position ID.
+- shopId: Optional shop ID.
+- machineId: Optional machine ID.
+- billId: Optional bill ID.
+- costumerId: Optional customer ID.
+- transactionId: Optional transaction ID.
+- discountId: Optional discount ID.
+- NAVCode: Optional NAV code.
+
+### Validation
+Each field is validated based on its defined pattern and type. Required fields must be provided; otherwise, an error will be thrown during validation.
+
+### JSON Structure
+The generated JSON object has the following structure:
+```json
+{
+    "M": {
+        "I": 1,
+        "V": 3,
+        "C": 1,
+        "E": 1735689599000,
+        "R": 0
+    },
+    "R": {
+        "B": "GIBAHUHB",
+        "N": "John Doe",
+        "I": "HU12345678901234567890123456"
+    },
+    "a": {
+        "V": 10000,
+        "C": "HUF"
+    },
+    "t": "Optional text",
+    "i": {
+        "p": "Optional Position ID",
+        "s": "Optional Shop ID",
+        "m": "Optional Machine ID",
+        "b": "Optional Bill ID",
+        "c": "Optional Customer ID",
+        "t": "Optional Transaction ID",
+        "d": "Optional Discount ID",
+        "n": "Optional NAV Code"
+    }
+}
+```
+
+
---
+++ dist/MNBQrV3.js
@@ -0,0 +1,423 @@
+"use strict"
+class MNBQrV3 {
+
+    //Verziószám
+    #schemaVersion = 3;
+
+    // Azonosító kódok
+    static HCT = 1;
+    static RTP = 2;
+
+    static REPETABLE = 1;
+    static UNREPETABLE = 0;
+
+    //Kulcsok
+    #KEY = Object.freeze({
+        BIC: "BIC",                         // Fizető fél vagy kedvezményezett BIC/BEI
+        NAME: "name",                       // Fizető fél vagy kedvezményezett neve
+        IBAN: "IBAN",                       // Fizető fél vagy kedvezményezett IBAN
+        DOMAIN: "domain",                   // Domain név a domain alapú validáció számára
+        VALUE: "value",                     // Összeg
+        CURRENCY: "currency",               // Pénznem
+        TEXT: "text",                       // Közlemény (nem struktúrált)
+        IDENTIFICATION: "identification",   // Azonosító kód, 1: HCT, 2: RTP
+        EXPIRY: "expiry",                   // Érvényességi idő (Unix timestamp)
+        REPEATABILITY: "repeatability",     // 0: QR kód vagy link csak egyszer olvasható be és utalható | 1: többször is végrehajtható
+        POSITION_ID: "positionId",          // Fizetési helyzet azonosító
+        SHOP_ID: "shopId",                  // Kereskedelmi egység, bolt azonosító
+        MACHINE_ID: "machineId",            // Kereskedelmi eszköz (POS, pénztárgép) azonosító
+        BILL_ID: "billId",                  // Számla vagy nyugta azonosító
+        COSTUMER_ID: "costumerId",          // Ügyfélazonosító
+        TRANSACTION_ID: "transactionId",    // Kedvezményezett belső tranzakcióazonosítója
+        DISCOUNT_ID: "discountId",          // Törzsvásárlói vagy kedvezményezett azonosítója
+        NAV_CODE: "NAVCode"                 // NAV ellenőrző kód
+    });
+
+    #fields = {
+        //Kedvezményezett Adatai
+        BIC : {
+            value: undefined,
+            type: 'string',
+            required: true,
+            pattern: /^[a-zA-Z]{8,11}$/, // /^[a-zA-Z]{11}$/,
+        },
+        name :{
+            value: undefined,
+            type: 'string',
+            required: true,
+            pattern: /^.{1,70}$/
+        },
+        IBAN : {
+            value: undefined,
+            type: 'string',
+            required: true,
+            pattern: /^[a-zA-Z0-9]{28}$/
+        },
+        domain : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^((?!-))(xn--)?[a-z0-9][a-z0-9\-_]{0,61}[a-z0-9]{0,}\\-.?((xn--)?([a-z0-9\-.]{1,61}|[a-z0-9-]{1,30})\\-.?[a-z]{2,})$/
+        },
+        // Összeg
+        value : {
+            value: undefined,
+            type: 'number',
+            required: true,
+            pattern: /^(10{7}|[1-9]\d{0,6}|0)$/
+        },
+        currency: {
+            value: undefined,
+            type: 'string',
+            required: true,
+            pattern: /^[a-zA-Z]{3}$/
+        },
+        text : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,70}$/,
+        },
+        // Meta adatok
+        identification : {
+            value: undefined,
+            type: 'number',
+            required: true,
+            pattern: /^.{1,2}$/
+        },
+        expiry: {
+            value: undefined,
+            type: 'number',
+            required: true,
+            pattern: /^\d{10,13}$/
+        },
+        repeatability: {
+            value: undefined,
+            type: 'number',
+            required: true,
+            pattern: /^.{0,1}$/
+        },
+        //Egyéb azonosítók
+        positionId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^[a-zA-Z0-9]{4}$/
+        },
+        shopId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+        },
+        machineId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+        },
+        billId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+        },
+        costumerId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+        },
+        transactionId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+
+        },
+        discountId : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+        },
+        NAVCode : {
+            value: undefined,
+            type: 'string',
+            required: false,
+            pattern: /^.{1,35}$/
+        },
+
+    };
+
+    constructor( schemaConf ) {
+
+        if( schemaConf !== undefined){
+
+            if(typeof schemaConf !== "object"){
+                Object.keys(schemaConf).forEach( ( _a) =>{
+                    if( this.#get(_a) !== false ){
+                        if( this.#isValid( _a, schemaConf[_a] ) === true ){
+                            this.#set( _a, schemaConf[_a] );
+                        }
+                    }
+                });
+            }else{
+                console.warn("Wrong schema config");
+            }
+
+        }
+    }
+
+    #get(_f){
+        if(this.#fields.hasOwnProperty(_f)){
+            return this.#fields[_f].value;
+        }else {
+            console.log(`Invalid field: ${_f}`);
+            return false;
+        }
+    }
+    #set(_f, _v){
+        if( this.#isValid(_f, _v) ){
+            if(this.#fields.hasOwnProperty(_f)){
+                this.#fields[_f].value = _v;
+            }else {
+                console.error(`Invalid ${_f} value: ${_v}`);
+            }
+        }
+    }
+    getBIC(){
+        return this.#get( this.#KEY.BIC );
+    }
+    setBIC(_v){
+        this.#set( this.#KEY.BIC, _v );
+    }
+    async getName(){
+        return this.#get( this.#KEY.NAME );
+    }
+    setName(_v){
+        this.#set( this.#KEY.NAME, _v );
+    }
+    async getIBAN(){
+        return this.#get( this.#KEY.IBAN );
+    }
+    setIBAN(_v){
+        this.#set( this.#KEY.IBAN, _v);
+    }
+    getDomain(){
+        return this.#get( this.#KEY.DOMAIN );
+    }
+    setDomain(_v){
+        this.#set( this.#KEY.DOMAIN, _v );
+    }
+    getValue(){
+        return this.#get( this.#KEY.VALUE );
+    }
+    setValue(_v){
+        this.#set( this.#KEY.VALUE, _v );
+    }
+    getCurrency(){
+        return this.#get( this.#KEY.CURRENCY );
+    }
+    setCurrency(_v){
+        this.#set( this.#KEY.CURRENCY, _v );
+    }
+    getText(){
+        return this.#get( this.#KEY.TEXT );
+    }
+    setText(_v){
+        this.#set( this.#KEY.TEXT, _v );
+    }
+    getIdentification(){
+        return this.#get( this.#KEY.IDENTIFICATION );
+    }
+    setIdentification(_v){
+        this.#set( this.#KEY.IDENTIFICATION, _v );
+    }
+    getExpiry(){
+        return this.#get( this.#KEY.EXPIRY );
+    }
+    setExpiry(_v){
+        this.#set( this.#KEY.EXPIRY, _v );
+    }
+    getRepeatability(){
+        return this.#get( this.#KEY.REPEATABILITY ) ;
+    }
+    setRepeatability(_v){
+        this.#set( this.#KEY.REPEATABILITY, _v );
+    }
+    getPositionId(){
+        return this.#get( this.#KEY.POSITION_ID );
+    }
+    setPositionId(_v){
+        this.#set( this.#KEY.POSITION_ID, _v );
+    }
+    getShopId(){
+        return this.#get( this.#KEY.SHOP_ID );
+    }
+    setShopId(_v){
+        this.#set( this.#KEY.SHOP_ID, _v );
+    }
+    getMachineId(){
+        return this.#get( this.#KEY.MACHINE_ID );
+    }
+    setMachineId(_v){
+        this.#set( this.#KEY.MACHINE_ID, _v );
+    }
+    getBillId(){
+        return this.#get( this.#KEY.BILL_ID );
+    }
+    setBillId(_v){
+        this.#set( this.#KEY.BILL_ID, _v );
+    }
+    getCostumerId(){
+        return this.#get( this.#KEY.COSTUMER_ID );
+    }
+    setCostumerId(_v){
+        this.#set( this.#KEY.COSTUMER_ID, _v );
+    }
+    getTransactionId(){
+        return this.#get( this.#KEY.TRANSACTION_ID );
+    }
+    setTransactionId(_v){
+        this.#set( this.#KEY.TRANSACTION_ID, _v);
+    }
+    getDiscountId(){
+        return this.#get( this.#KEY.DISCOUNT_ID );
+    }
+    setDiscountId(_v){
+        return this.#set( this.#KEY.DISCOUNT_ID, _v );
+    }
+    getNAVCode(){
+        return this.#get( this.#KEY.NAV_CODE );
+    }
+    setNAVCode(_v){
+        this.#set( this.#KEY.NAV_CODE, _v );
+    }
+
+    #isValid( _f, _v ){
+        try{
+
+            if(!this.#fields.hasOwnProperty(_f)){
+                console.error(`Invalid field: '${_f}'`);
+                return false;
+            }
+
+            if(typeof _v === 'undefined'){
+                return true;
+            }
+
+            if(typeof _v == "string"){
+                _v = _v.trim();
+            }
+
+            if(typeof _v !== this.#fields[_f].type){
+                console.error(`Wrong data type for '${_f}' excepted '${this.#fields[_f].type}' or 'undefined' but got '${typeof _v}'`)
+                return false;
+            }
+
+            if(!_v.toString().match(this.#fields[_f].pattern)){
+                console.error(`Value '${_v}' dont match the filed pattern Field: '${_f}' Pattern: '${this.#fields[_f].pattern}'`)
+                return false;
+            }
+
+            return true;
+
+        }catch (err){
+            console.error(err);
+            return false;
+        }
+    }
+    #schemaIsValid(){
+        try{
+
+            Object.keys(this.#fields).forEach( _f => {
+
+                if(typeof this.#fields[_f].value === "string"){
+                    return this.#fields[_f].value = this.#fields[_f].value.trim();
+                }
+
+                if(this.#fields[_f].required && [null, undefined, ''].includes(this.#fields[_f].value)){
+                    throw new Error(`Required filed '${_f}' missing`);
+                }
+
+                if(this.#fields[_f].value !== undefined && typeof this.#fields[_f].value !== this.#fields[_f].type){
+                    throw new Error(`Wrong data type for '${_f}' excepted ${this.#fields[_f].type} but got ${typeof this.#fields[_f].value}`);
+                }
+
+                if(this.#fields[_f].value !== undefined){
+
+                    if(!this.#fields[_f].value.toString().match(this.#fields[_f].pattern)){
+                        throw new Error(`${_f} value malformed`);
+                    }
+
+                }
+
+            });
+
+            return true;
+
+        }catch (err){
+            console.error(err);
+            return false;
+        }
+    }
+    generateObject(){
+        try{
+
+            if( this.#schemaIsValid() ){
+
+                const json = {
+                    "M": {
+                        "I": this.#fields.identification.value,
+                        "V": this.#schemaVersion,
+                        "C": 1,
+                        "E": this.#fields.expiry.value,
+                        "R": this.#fields.repeatability.value
+                    },
+                    "R": {
+                        "B": this.#fields.BIC.value,
+                        "N": this.#fields.name.value,
+                        "I": this.#fields.IBAN.value
+                    },
+                    "a": {
+                        "V": this.#fields.value.value,
+                        "C": this.#fields.currency.value,
+                    },
+                    "t": this.#fields.text.value,
+                    "i": {
+                        "p": this.#fields.positionId.value,
+                        "s": this.#fields.shopId.value,
+                        "m": this.#fields.machineId.value,
+                        "b": this.#fields.billId.value,
+                        "c": this.#fields.costumerId.value,
+                        "t": this.#fields.transactionId.value,
+                        "d": this.#fields.discountId.value,
+                        "n": this.#fields.NAVCode.value
+                    }
+                }
+
+                return json;
+
+            }
+
+            return false;
+
+        }catch (err){
+            console.error(err);
+        }
+
+    };
+
+    generateJSON(){
+        try{
+            return JSON.stringify(
+                this.generateObject()
+            );
+        }catch (err){
+            console.error(err);
+        }
+
+    };
+
+}
